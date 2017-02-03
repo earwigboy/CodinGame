63212764c7115464e51d4c0c6cc4984febbaef3c
earwigboy <earwigboy@gmail.com>
2017-02-03 09:38:21 +0000

    Working solution.

--- Medium/Teads Sponsored Contest/Teads Sponsored Contest/main.cpp
+++ Medium/Teads Sponsored Contest/Teads Sponsored Contest/main.cpp
@@ -4,22 +4,26 @@
 #include <map>
 #include <algorithm>
 #include <set>
+//#include <fstream>
 
 using namespace std;
 
 
 int main()
 {
+    //ifstream infile("input");
     map<int,vector<int>> adjList;
     map<int,vector<int>>::iterator adjListIterator;
     int n; // the number of adjacency relations
     cin >> n; cin.ignore();
+    //infile >> n;
     cerr << n << endl;
     for (int i = 0; i < n; i++) {
         
         int xi; // the ID of a person which is adjacent to yi
         int yi; // the ID of a person which is adjacent to xi
         cin >> xi >> yi; cin.ignore();
+        //infile >> xi >> yi;
         
         //
         if (adjList.count(xi) == 0){
@@ -35,38 +39,50 @@ int main()
             nodes.push_back(xi);
             adjList[yi] = nodes;
         } else {
-            adjList[yi].push_back(yi);
+            adjList[yi].push_back(xi);
         }
         cerr << xi << " " << yi << endl;
     }
-    /*
+    
     for (adjListIterator = adjList.begin(); adjListIterator != adjList.end(); adjListIterator++){
         cerr << adjListIterator->first << " | ";
         for (vector<int>::iterator it = adjListIterator->second.begin(); it != adjListIterator->second.end(); it++ ){
             cerr << *it << ", ";
         }
         cerr << endl;
-    }*/
+    }
     
     int relays = 0;
-    
+    //cerr << "size = " << adjList.size() << endl;
     while (adjList.size() > 1){
         vector<int> removeKeys;
+        vector<int> singleKeys;
         for (adjListIterator= adjList.begin(); adjListIterator != adjList.end(); adjListIterator++){
-            int key = adjListIterator->first;
-            vector<int> value = adjListIterator->second;
+            if (adjListIterator->second.size() == 1){
+                singleKeys.push_back(adjListIterator->first);
+            }
+        }
+        //for (adjListIterator= adjList.begin(); adjListIterator != adjList.end(); adjListIterator++){
+        for (vector<int>::iterator vit = singleKeys.begin(); vit != singleKeys.end(); vit++){
+            //int key = adjListIterator->first;
+            //vector<int> value = adjListIterator->second;
+            int key = *vit;
+            vector<int> value = adjList[key];
             if (value.size() == 1) {
                 // remove the reference from the node referenced in the key
                 vector<int>::iterator it = find(adjList[value[0]].begin(), adjList[value[0]].end(), key);
                 if (it != adjList[value[0]].end()){
                     adjList[value[0]].erase(it);
                 }
-                adjListIterator->second.clear();
-                removeKeys.push_back(adjListIterator->first);
+                //adjListIterator->second.clear();
+                adjList[key].clear();
+                removeKeys.push_back(key);
+                //removeKeys.push_back(adjListIterator->first);
             }
         }
         for(int i=0; i < removeKeys.size(); i++)
             adjList.erase(removeKeys[i]);
+        //cerr << "size = " << adjList.size() << endl;
         
         relays++;
         /*
