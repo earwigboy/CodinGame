259dec04768b9de7fb762bca787a73585e426a7c
earwigboy <earwigboy@gmail.com>
2017-02-03 10:10:27 +0000

    Cleaned up final solution

--- Medium/Teads Sponsored Contest/Teads Sponsored Contest/main.cpp
+++ Medium/Teads Sponsored Contest/Teads Sponsored Contest/main.cpp
@@ -3,29 +3,24 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-#include <set>
-//#include <fstream>
+
 
 using namespace std;
 
 
 int main()
 {
-    //ifstream infile("input");
     map<int,vector<int>> adjList;
     map<int,vector<int>>::iterator adjListIterator;
     int n; // the number of adjacency relations
     cin >> n; cin.ignore();
-    //infile >> n;
-    cerr << n << endl;
+    
     for (int i = 0; i < n; i++) {
         
         int xi; // the ID of a person which is adjacent to yi
         int yi; // the ID of a person which is adjacent to xi
         cin >> xi >> yi; cin.ignore();
-        //infile >> xi >> yi;
-        
-        //
+     
         if (adjList.count(xi) == 0){
             vector<int> nodes(0);
             nodes.push_back(yi);
@@ -41,19 +36,10 @@ int main()
         } else {
             adjList[yi].push_back(xi);
         }
-        cerr << xi << " " << yi << endl;
-    }
-    
-    for (adjListIterator = adjList.begin(); adjListIterator != adjList.end(); adjListIterator++){
-        cerr << adjListIterator->first << " | ";
-        for (vector<int>::iterator it = adjListIterator->second.begin(); it != adjListIterator->second.end(); it++ ){
-            cerr << *it << ", ";
-        }
-        cerr << endl;
     }
     
     int relays = 0;
-    //cerr << "size = " << adjList.size() << endl;
+    
     while (adjList.size() > 1){
         vector<int> removeKeys;
         vector<int> singleKeys;
@@ -62,10 +48,8 @@ int main()
                 singleKeys.push_back(adjListIterator->first);
             }
         }
-        //for (adjListIterator= adjList.begin(); adjListIterator != adjList.end(); adjListIterator++){
         for (vector<int>::iterator vit = singleKeys.begin(); vit != singleKeys.end(); vit++){
-            //int key = adjListIterator->first;
-            //vector<int> value = adjListIterator->second;
+        
             int key = *vit;
             vector<int> value = adjList[key];
             if (value.size() == 1) {
@@ -74,36 +58,16 @@ int main()
                 if (it != adjList[value[0]].end()){
                     adjList[value[0]].erase(it);
                 }
-                //adjListIterator->second.clear();
+        
                 adjList[key].clear();
                 removeKeys.push_back(key);
-                //removeKeys.push_back(adjListIterator->first);
             }
         }
         for(int i=0; i < removeKeys.size(); i++)
             adjList.erase(removeKeys[i]);
-        //cerr << "size = " << adjList.size() << endl;
         
         relays++;
-        /*
-        for (adjListIterator = adjList.begin(); adjListIterator != adjList.end(); adjListIterator++){
-            cerr << adjListIterator->first << " | ";
-            for (vector<int>::iterator it = adjListIterator->second.begin(); it != adjListIterator->second.end(); it++ ){
-                cerr << *it << ", ";
-            }
-            cerr << endl;
-        }
-         */
+        
     }
-    /*
-    for (adjListIterator = adjList.begin(); adjListIterator != adjList.end(); adjListIterator++){
-        cerr << adjListIterator->first << " | ";
-        for (vector<int>::iterator it = adjListIterator->second.begin(); it != adjListIterator->second.end(); it++ ){
-            cerr << *it << ", ";
-        }
-        cerr << endl;
-    }
-     */
     cout << relays << endl;
-    
 }
